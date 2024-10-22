47ad9182ec4c3fcfdca12cdd0d015d36f550d59a
johnc365 <johnrcaldwell123@gmail.com>
2024-10-22 09:32:05 -0500

    finished setting up files and directories before writing main code

---
+++ src/db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES
+('legal'),
+('operations'),
+('sales');
+('management')
+
+INSERT INTO role (title, salary)
+VALUES
+('lawyer', 200000, 1),
+('laborer', 40000, 2),
+('salesman', 80000, 3),
+('manager', 120000, 4);
+
+INSERT INTO employee (first_name, last_name)
+('Don', 'Roberts', 1),
+('Jack', 'Hanson', 2, 4),
+('Robert', 'Wilson', 3),
+('Gary', 'Green', 4),
+('Henry', 'Daws', 2, 4);
